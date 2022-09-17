df62bd84ebff5d9b98b50ed6a9351aac14d8dcc1
Vittorio Lo Mele <vittorio@mrbackslash.it>
2022-09-17 11:29:17 +0200

    navbar edits and new credit page

--- src/main/java/it/schoolboard/app/controller/PortalController.java
+++ src/main/java/it/schoolboard/app/controller/PortalController.java
@@ -1,6 +1,8 @@
 package it.schoolboard.app.controller;
 
 import it.schoolboard.app.utility.SSOUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,11 +11,55 @@ import org.springframework.web.bind.annotation.GetMapping;
 // this controller handles all calls to the pages of the "portal"
 @Controller
 public class PortalController {
-    // homepage, contains the profile selector
+
+    @Autowired
+    BuildProperties buildProperties;
+
+    // homepage, contains the profile selector or the public content
     @GetMapping("/")
     public String index(Model model) {
         SSOUser user = new SSOUser(SecurityContextHolder.getContext());
         model.addAttribute("user", user);
         return "pages/portal/index";
+    }
+
+    // pages that changes the avatar of the user in the whole application
+    @GetMapping("/changeAvatar")
+    public String changeAvatar(Model model) {
+        SSOUser user = new SSOUser(SecurityContextHolder.getContext());
+        model.addAttribute("user", user);
+        return "pages/portal/changeAvatar";
+    }
+
+    @GetMapping("/info/privacy")
+    public String privacyPolicy(Model model) {
+        SSOUser user = new SSOUser(SecurityContextHolder.getContext());
+        model.addAttribute("user", user);
+        return "pages/portal/info/privacy";
+    }
+
+    @GetMapping("/info/legalNotes")
+    public String legalNotes(Model model) {
+        SSOUser user = new SSOUser(SecurityContextHolder.getContext());
+        model.addAttribute("user", user);
+        return "pages/portal/info/legalNotes";
+    }
+
+    @GetMapping("/info/cookies")
+    public String cookiePolicy(Model model) {
+        SSOUser user = new SSOUser(SecurityContextHolder.getContext());
+        model.addAttribute("user", user);
+        return "pages/portal/info/cookies";
+    }
+
+    @GetMapping("/info/credits")
+    public String credits(Model model) {
+        SSOUser user = new SSOUser(SecurityContextHolder.getContext());
+        model.addAttribute("user", user);
+
+        // get software version / BUILD
+        model.addAttribute("buildProp", buildProperties);
+
+        return "pages/portal/info/credits";
     }
 }
